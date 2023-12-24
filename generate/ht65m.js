696b358f1d7f6df08479e9d2c2080fe0878f551b
Lorentzos <141639738+Lorentzos@users.noreply.github.com>
2023-12-24 10:15:01 +0200

    Update ht65m.js

--- generate/ht65m.js
+++ generate/ht65m.js
@@ -1,4 +1,38 @@
 
 // Lorentzos Encryption --()
 
-var _0x3cacc1=_0x3b64;function _0x3b64(_0x428663,_0x574181){var _0x3abff2=_0x3abf();return _0x3b64=function(_0x3b6461,_0x2c7ba3){_0x3b6461=_0x3b6461-0x9e;var _0xf0c962=_0x3abff2[_0x3b6461];return _0xf0c962;},_0x3b64(_0x428663,_0x574181);}function _0x3abf(){var _0x4f3db8=['1233630EWGyVF','2988254yLjbnn','54bybrEM','822021YdcYTW','245Ylwpkd','write','14VmeWRS','3575049HyPiQw','8310PjDPRT','4WCIbUT','36525929WouHvU','2605816tXzGti'];_0x3abf=function(){return _0x4f3db8;};return _0x3abf();}(function(_0x4c01cf,_0xf2b2af){var _0x444827=_0x3b64,_0x3ab78f=_0x4c01cf();while(!![]){try{var _0x15b3cf=-parseInt(_0x444827(0x9e))/0x1+-parseInt(_0x444827(0xa8))/0x2+parseInt(_0x444827(0xa2))/0x3*(parseInt(_0x444827(0xa4))/0x4)+parseInt(_0x444827(0x9f))/0x5*(parseInt(_0x444827(0xa3))/0x6)+parseInt(_0x444827(0xa1))/0x7*(-parseInt(_0x444827(0xa6))/0x8)+-parseInt(_0x444827(0xa9))/0x9*(parseInt(_0x444827(0xa7))/0xa)+parseInt(_0x444827(0xa5))/0xb;if(_0x15b3cf===_0xf2b2af)break;else _0x3ab78f['push'](_0x3ab78f['shift']());}catch(_0x16b695){_0x3ab78f['push'](_0x3ab78f['shift']());}}}(_0x3abf,0xd4f73),document[_0x3cacc1(0xa0)](unescape('%3C%21DOCTYPE%20html%3E%0A%3Chtml%20lang%3D%22en%22%3E%0A%3Chead%3E%0A%3Cmeta%20charset%3D%22UTF-8%22%3E%0A%3Ctitle%3ELorentzos%3C/title%3E%0A%3Cmeta%20property%3D%22og%3Atitle%22%20content%3D%22Lorentzos%22%3E%0A%3Cmeta%20property%3D%22og%3Adescription%22%20content%3D%22Lorentzos%20Landing%20Page%22%3E%0A%3Cmeta%20property%3D%22og%3Aimage%22%20content%3D%22https%3A//lorentzos.digital/images/logoBlue.png%22%3E%0A%3Cmeta%20property%3D%22og%3Aurl%22%20content%3D%22https%3A//lorentzos.digital%22%3E%0A%3Cmeta%20name%3D%22keywords%22%20content%3D%22Lorentzos%2C%20lorentzos%22%3E%0A%3Cmeta%20name%3D%22author%22%20content%3D%22Lorentzos%22%3E%0A%3Cmeta%20name%3D%22description%22%20content%3D%22Lorentzos%20Landing%20Page%22%3E%0A%3Cmeta%20name%3D%22viewport%22%20content%3D%22width%3Ddevice-width%2C%20initial-scale%3D1%22%3E%0A%3Clink%20rel%3D%22icon%22%20href%3D%22https%3A//lorentzos.digital/images/logo.png%22%20type%3D%22https%3A//lorentzos.digital/images/logo.png%22%3E%0A%3Clink%20rel%3D%22shortcut%20icon%22%20href%3D%22https%3A//lorentzos.digital/images/logo.png%22%20type%3D%22https%3A//lorentzos.digital/images/logo.png%22%3E%0A%3C/head%3E%0A%0A%0A%3Cbody%3E%0A%0A%3Csection%20class%3D%22animate__animated%20animate__fadeInUp%22%3E%0A%20%20%20%20%3Ch3%20id%3D%22_%2C4r%22%3E%3C/h3%3E%0A%20%20%20%20%3Ch1%3EQR%20Code%20Generator%3C/h1%3E%0A%0A%0A%20%20%20%20%3Clabel%20for%3D%22linkInput%22%3E%3Ch4%3EEnter%20Link%3A%3C/h4%3E%3C/label%3E%0A%20%20%20%20%3Cinput%20type%3D%22text%22%20id%3D%22linkInput%22%20placeholder%3D%22Enter%20a%20link%22%3E%0A%0A%20%20%20%20%3Cbutton%20onclick%3D%22generateQRCode%28%29%22%3EGenerate%20QR%20Code%3C/button%3E%0A%3C/section%3E%0A%0A%3Cdiv%20id%3D%22qrcode-container%22%20class%3D%22animate__animated%20animate__fadeInUp%22%3E%0A%20%20%20%20%3Cdiv%20id%3D%22qrcode%22%3E%3C/div%3E%0A%3C/div%3E%0A%0A%3Ca%20id%3D%22downloadLink%22%20download%3D%22qrcode.png%22%20class%3D%22animate__animated%20animate__fadeInUp%22%3EDownload%20QR%20Code%3C/a%3E%0A%0A%3C/body%3E%0A%3C/html%3E%22%29%29%3B')));
+function _0x38d5() {
+    var _0x4a19df = ['1745xTqHLG', '154264IvtgLQ', '%3C%21DOCTYPE%20html%3E%0A%3Chtml%20lang%3D%22en%22%3E%0A%3Chead%3E%0A%20%20%20%20%3Cmeta%20charset%3D%22UTF-8%22%3E%0A%20%20%20%20%3Cmeta%20name%3D%22viewport%22%20content%3D%22width%3Ddevice-width%2C%20initial-scale%3D1.0%22%3E%0A%20%20%20%20%3Ctitle%20id%3D%22_%2C5r%22%3ELorentzos%20QR%20Code%20Generator%3C/title%3E%0A%3C/head%3E%0A%3Cbody%3E%0A%0A%3Csection%20class%3D%22animate__animated%20animate__fadeInUp%22%3E%0A%20%20%20%20%3Ch3%20id%3D%22_%2C4r%22%3E%3C/h3%3E%0A%20%20%20%20%3Ch1%3EQR%20Code%20Generator%3C/h1%3E%0A%0A%0A%20%20%20%20%3Clabel%20for%3D%22linkInput%22%3E%3Ch4%3EEnter%20Link%3A%3C/h4%3E%3C/label%3E%0A%20%20%20%20%3Cinput%20type%3D%22text%22%20id%3D%22linkInput%22%20placeholder%3D%22Enter%20a%20link%22%3E%0A%0A%20%20%20%20%3Cbutton%20onclick%3D%22generateQRCode%28%29%22%3EGenerate%20QR%20Code%3C/button%3E%0A%3C/section%3E%0A%0A%3Cdiv%20id%3D%22qrcode-container%22%20class%3D%22animate__animated%20animate__fadeInUp%22%3E%0A%20%20%20%20%3Cdiv%20id%3D%22qrcode%22%3E%3C/div%3E%0A%3C/div%3E%0A%0A%3Ca%20id%3D%22downloadLink%22%20download%3D%22qrcode.png%22%20class%3D%22animate__animated%20animate__fadeInUp%22%3EDownload%20QR%20Code%3C/a%3E%0A%0A%3C/body%3E%0A%3C/html%3E', '6Kjeqwf', '612700bNVHcb', '5280UKZGYc', '6MtHkAx', '3410109MecluD', '1631457QBOFYg', '8655108evaAwC', '20XlINWi', '12849688gIyuvs', '9948FrTBsb', 'write'];
+    _0x38d5 = function() {
+        return _0x4a19df;
+    }
+    ;
+    return _0x38d5();
+}
+function _0x2d4a(_0x9a4853, _0x1c5738) {
+    var _0x38d541 = _0x38d5();
+    return _0x2d4a = function(_0x2d4a7a, _0x37a511) {
+        _0x2d4a7a = _0x2d4a7a - 0x11f;
+        var _0x533248 = _0x38d541[_0x2d4a7a];
+        return _0x533248;
+    }
+    ,
+    _0x2d4a(_0x9a4853, _0x1c5738);
+}
+var _0x16b5fa = _0x2d4a;
+(function(_0x2bff57, _0x249ab6) {
+    var _0x49ab1d = _0x2d4a
+      , _0x203eeb = _0x2bff57();
+    while (!![]) {
+        try {
+            var _0x25a502 = -parseInt(_0x49ab1d(0x123)) / 0x1 + parseInt(_0x49ab1d(0x125)) / 0x2 * (-parseInt(_0x49ab1d(0x127)) / 0x3) + parseInt(_0x49ab1d(0x12b)) / 0x4 * (-parseInt(_0x49ab1d(0x11f)) / 0x5) + -parseInt(_0x49ab1d(0x122)) / 0x6 * (parseInt(_0x49ab1d(0x128)) / 0x7) + -parseInt(_0x49ab1d(0x12a)) / 0x8 + -parseInt(_0x49ab1d(0x126)) / 0x9 * (-parseInt(_0x49ab1d(0x129)) / 0xa) + -parseInt(_0x49ab1d(0x120)) / 0xb * (-parseInt(_0x49ab1d(0x124)) / 0xc);
+            if (_0x25a502 === _0x249ab6)
+                break;
+            else
+                _0x203eeb['push'](_0x203eeb['shift']());
+        } catch (_0x4c285a) {
+            _0x203eeb['push'](_0x203eeb['shift']());
+        }
+    }
+}(_0x38d5, 0xedb13),
+document[_0x16b5fa(0x12c)](unescape(_0x16b5fa(0x121))));
